6ea08fb261195e74861733a84c8822be53e0f3bf
jake25fuller <jake25fuller@gmail.com>
2020-03-23 14:07:45 -0400

    Jake Fuller commit 2

---
+++ Command.java
@@ -0,0 +1,10 @@
+/**
+ * Command.java - simple interface for each command class to inherit
+ * @author Jake Fuller
+ */
+package csce247.assignments.command;
+
+public interface Command {
+
+	public void execute();
+}
